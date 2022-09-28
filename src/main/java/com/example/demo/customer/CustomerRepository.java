4a226742714ab5bc3c9e07d72446d64063b3d50d
Joaquin <joacomda@gmail.com>
2022-09-28 16:07:19 +0200

    Bean and annotations

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -2,19 +2,21 @@ package com.example.demo.customer;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 
-import java.util.Collections;
 import java.util.List;
-@Component
+@Repository
 @Primary
 public class CustomerRepository implements CustomerRepo{
 
     @Override
     public List<Customer> getCustomers() {
         // TODO connect to real db
-        return Collections.singletonList(
-                new Customer(1L, "TODO, Implement real db")
+        return List.of(
+                new Customer(1L, "TODO, Implement real db"),
+                new Customer(2L, "Joaquin Grandiccelli")
         );
     }
+
 }
