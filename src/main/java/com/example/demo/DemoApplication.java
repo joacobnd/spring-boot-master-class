7c6e909a2f8ce1ad99b24f2c7dcaaf29c56bb228
Joaquin <joacomda@gmail.com>
2022-09-28 09:31:34 +0200

    Spring Boot Master Class

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	Customer getCustomer() {
+		return new Customer(1L, "James Bond");
+	}
+
+	class Customer {
+		private final Long id;
+		private final String name;
+
+		Customer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+
+
+}
