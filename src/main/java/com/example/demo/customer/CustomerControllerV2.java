c2394a4b4def69a2d6abfa5a254c1debbb557ce3
Joaquin <joacomda@gmail.com>
2022-09-30 10:44:42 +0200

    RequestMapping and API v2

--- src/main/java/com/example/demo/customer/CustomerControllerV2.java
+++ src/main/java/com/example/demo/customer/CustomerControllerV2.java
@@ -3,22 +3,26 @@ package com.example.demo.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-@RequestMapping(path = "api/v1/customer")
+@RequestMapping(path = "api/v2/customer")
 @RestController
-public class CustomerController {
+public class CustomerControllerV2 {
 
     private final CustomerService customerService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerControllerV2(CustomerService customerService) {
         this.customerService = customerService;
     }
 
     @GetMapping(value = "all")
     List<Customer> getCustomer() {
-        return customerService.getCustomer();
+        return Collections.singletonList(
+                new Customer(0L, "v2", "v2")
+        );
     }
 
     @PostMapping
