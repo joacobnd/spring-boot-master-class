0a71e2bd284949f8a5e0619a0be2fb97dde8b333
Joaquin <joacomda@gmail.com>
2022-10-04 16:10:56 +0200

    ControllerAdvice and ExceptionHandler

--- src/main/java/com/example/demo/customer/CustomerControllerV2.java
+++ src/main/java/com/example/demo/customer/CustomerControllerV2.java
@@ -1,5 +1,6 @@
 package com.example.demo.customer;
 
+import com.example.demo.exception.ApiRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +29,11 @@ public class CustomerControllerV2 {
     @GetMapping(path = "{customerId}")    // = propiedad "id" de la clase Customer por medio de  @JsonProperty se le da el nombre customerId
     Customer getCustomer(@PathVariable("customerId") Long id) {
         return customerService.getCustomer(id);
+    }
+
+    @GetMapping(path = "{customerId}/exception")    // = propiedad "id" de la clase Customer por medio de  @JsonProperty se le da el nombre customerId
+    Customer getCustomerException(@PathVariable("customerId") Long id) {
+        throw new ApiRequestException("ApiRequestException for customer " + id);
     }
 
     @PostMapping
